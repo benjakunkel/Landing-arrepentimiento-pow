eaa98a1b3158f0df97589fd937e6cb92c5a9f8fd
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-12-10 12:20:54 -0300

    asiahga's

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@
 $(function() {
 
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 650) {
+        if($(window).scrollTop() > 150) {
             // Cuando el scroll top se a de 700 agrego:
             //Clase .active con fondo blanco
             $(".header").addClass("active");
