994b9ad6e085965978aaa20bc94effce01c56c5c
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-12-14 15:36:05 -0300

    probando 3 destinatarios php

--- enviar2.php
+++ enviar2.php
@@ -1,7 +1,7 @@
 <?php
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$info = $_POST['mensaje'];
 $empresa = $_POST['empresa'];
 
 $header = 'From: ' . $mail . " \r\n";
@@ -10,15 +10,16 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Nombre:" . $nombre . ",\r\n";
-$empresa = "Empresa:" . $empresa . ",\r\n";
-$mensaje .= "Mail: " . $mail . " \r\n";
+$mensaje .= "Empresa:" . $empresa . ",\r\n";
+$mensaje .= "Mail de contacto: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$fecha .= "Enviado el " . date('d/m/Y', time());
+$mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'benjamin@pow.la';
-$asunto = 'Nuevo contacto LANDING POW';
 
-mail($para, $asunto, utf8_decode($fecha), $header);
+$dest = 'martin@pow.la, guido@pow.la, felipe@pow.la ';
+$asunto = 'Contacto - LANDING POW';
+
+mail($dest, $asunto, utf8_decode($mensaje), $header);
 
 header("Location:index.html");
 ?>
